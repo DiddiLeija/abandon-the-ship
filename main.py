8002c60997c2683add3f1ccc71249b57114bbcb6
Diego Ramirez <dr01191115@gmail.com>
2022-11-04 13:13:03 -0600

    Add a small Python file to start, and a requirements file

---
+++ main.py
@@ -0,0 +1,17 @@
+import pyxel
+
+
+class App:
+    "The main class that puts everything together."
+
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+
+
+if __name__ == '__main__':
+    App()
