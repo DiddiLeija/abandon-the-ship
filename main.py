86973f93c2a9586594c342cedf4e9ac06e61c0f3
Diego Ramirez <dr01191115@gmail.com>
2022-11-05 14:52:04 -0600

    Implement some real code into the main file

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ class App:
     "The main class that puts everything together."
 
     def __init__(self):
-        pass
+        pyxel.load("resource.pyxres")
+        self.startup()
+
+    def startup(self):
+        pyxel.run(self.update, self.run)
 
     def update(self):
-        pass
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
 
     def draw(self):
+        pass
 
 
 if __name__ == '__main__':
+    pyxel.init(180, 180, title="Abandon the ship!")
     App()
